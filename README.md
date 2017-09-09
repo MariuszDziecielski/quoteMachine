60d4eefd8496d963d3a2c83d2f1917c401c18a12
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-09-09 19:58:24 +0200

    Remove unnecessary spaces & change js

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ###### - Install [**Node.js**](https://nodejs.org/en/download/)
 
-###### - Open a command line window (Windows) or a terminal window (Mac & Linux) 
+###### - Open a command line window (Windows) or a terminal window (Mac & Linux)
 
 ###### - Type `npm install -g grunt-cli`, and press Enter to install Grunt's command line interface (CLI) globally
 
 ###### - Type `npm install`, and press Enter
 
-###### - Type `grunt`, and press Enter 
+###### - Type `grunt`, and press Enter
 
 ###### Enjoy!
--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
 $(function () {
-    const prefix = "https://cors-anywhere.herokuapp.com/",
-        tweetLink = "https://twitter.com/intent/tweet?text=",
-        quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-    function getQuote() {
-        $.getJSON(prefix + quoteUrl, createTweet);
-        $.ajaxSetup({
-            cache: false
+    function QuoteMachine(prefix, tweetLink, quoteUrl) {
+        this.prefix = prefix;
+        this.tweetLink = tweetLink;
+        this.quoteUrl = quoteUrl;
+        $('.trigger').click(() => {
+            this.getQuote();
         });
     }
-    function createTweet(input) {
-        const data = input[0],
-            quoteText = $(data.content).text().trim();
-        let quoteAuthor = data.title;
-        if (!quoteAuthor.length) {
-            quoteAuthor = "Unknown author";
+    QuoteMachine.prototype = {
+        getQuote: function () {
+            $.getJSON(`${this.prefix}${this.quoteUrl}`, this.createTweet);
+            $.ajaxSetup({
+                cache: false
+            });
+        },
+        createTweet: function (input) {
+            const data = input[0],
+                quoteText = $(data.content).text().trim();
+            let quoteAuthor = data.title;
+            if (!quoteAuthor.length) {
+                quoteAuthor = "Unknown author";
+            }
+            const tweetText = `Quote of the day - ${quoteText} Author: ${quoteAuthor}`;
+            if (tweetText.length > 140) {
+                quoteMachine.getQuote();
+            } else {
+                const tweet = `${quoteMachine.tweetLink}${encodeURIComponent(tweetText)}`;
+                $('.quote p').text(quoteText);
+                $('.author').text(`Author: ${quoteAuthor}`);
+                $('.tweet').attr('href', tweet);
+            }
         }
-        const tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-        if (tweetText.length > 140) {
-            getQuote();
-        } else {
-            const tweet = tweetLink + encodeURIComponent(tweetText);
-            $('.quote p').text(quoteText);
-            $('.author').text("Author: " + quoteAuthor);
-            $('.tweet').attr('href', tweet);
-        }
-    }
-    getQuote();
-    $('.trigger').click(function () {
-        getQuote();
-    });
+    };
+    const quoteMachine = new QuoteMachine("https://cors-anywhere.herokuapp.com/", "https://twitter.com/intent/tweet?text=", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1");
+    quoteMachine.getQuote();
 });
